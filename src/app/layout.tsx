6a70b2adb7f8f107e3e043de9baff47fac84e595
prabhath <prabhathc@icloud.com>
2024-12-07 01:23:29 -0500

    Update src/app/layout.tsx [BoltSync]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="bg-upa-dark">
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
